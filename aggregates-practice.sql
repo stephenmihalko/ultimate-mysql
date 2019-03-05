d7ae61ad36969f0863b953b16326a47962944ac5
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-05 16:51:03 -0500

    Update aggregates-practice.sql
    Change first select
    Print total books in stock
    Add average release year per author

--- aggregates-practice.sql
+++ aggregates-practice.sql
@@ -1,5 +1,5 @@
 -- Print the number of books in the database
-SELECT SUM(stock_quantity)
+SELECT COUNT(*)
 FROM Books;
 
 -- Print how many books were released in each year
@@ -7,3 +7,12 @@ SELECT released_year,  COUNT(*)
 FROM Books
 GROUP BY released_year
 ORDER BY released_year ASC;
+
+-- Print the total number of books in stock
+SELECT SUM(stock_quantity)
+FROM Books;
+
+-- Find the average released year for each author
+SELECT author_fname, author_lname, AVG(released_year)
+FROM Books
+GROUP BY author_fname, author_lname;
