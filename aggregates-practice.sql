8146b78fa8e2b4aa75be608ed1a2898d3997d63a
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-05 16:47:40 -0500

    Update aggregates-practice.sql
    Updates to print books released each year

--- aggregates-practice.sql
+++ aggregates-practice.sql
@@ -3,6 +3,7 @@ SELECT SUM(stock_quantity)
 FROM Books;
 
 -- Print how many books were released in each year
-SELECT COUNT(*)
+SELECT released_year,  COUNT(*)
 FROM Books
-GROUP BY release_year;
+GROUP BY released_year
+ORDER BY released_year ASC;
