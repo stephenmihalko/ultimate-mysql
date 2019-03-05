7ef6dd2f99e3f5bb91ba41e29e395eaaff929fe6
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-03-05 10:42:37 -0500

    Create refining-selections.sql

---
+++ refining-selections.sql
@@ -0,0 +1,37 @@
+-- Select all titles that contain 'stories'
+SELECT title
+FROM Books
+WHERE title LIKE '%stories%';
+
+-- Find the book with the most pages
+SELECT title, pages
+FROM Books
+ORDER BY pages DESC
+LIMIT 1;
+
+-- Print summary containing title and year for 3 most recent books
+SELECT CONCAT(title, ' - ', year) AS summary
+FROM Books
+ORDER BY year DESC
+LIMIT 3;
+
+-- Find books with a space in the author's last name
+SELECT title, author_lname
+FROM Books
+WHERE author_lname LIKE '% %';
+
+-- Find the three books with the lowest stock
+SELECT title, released_year, stock_quantity
+FROM Books
+ORDER BY stock_quantity
+LIMIT 3;
+
+-- Print title and last name, sorted by author then title
+SELECT title, author_lname
+FROM Books
+ORDER BY author_lname, title;
+
+-- DO SOME YELLING!!!
+SELECT CONCAT('MY FAVORITE AUTHOR IS ', UPPER(author_fname), ' ', UPPER(author_lname), '!')
+FROM Books
+ORDER BY author_lname;
