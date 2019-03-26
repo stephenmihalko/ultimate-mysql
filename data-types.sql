b98b675608f5dd4cb9d5a4671a1260215a2830b2
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-25 20:57:06 -0400

    Create data-types.sql
    Write comments for exercises and questions

---
+++ data-types.sql
@@ -0,0 +1,23 @@
+-- What's a good use for CHAR instead of VARCHAR?
+
+-- Fill in the blanks in the SQL code below
+CREATE TABLE Inventory
+(
+    item_name ___________________,
+    price ___________________,
+    quantity ___________________
+);
+
+-- What's the difference between DATETIME and TIMESTAMP?
+
+-- Print the current time
+
+-- Print the current date (not the time)
+
+-- Print out the current day of the week (number and name)
+
+-- Print today's date as mm/dd/yyyy
+
+-- Print the time as <Month name> <ordinal date> at <time>
+
+-- Create a tweets table that stores content, username, time created
