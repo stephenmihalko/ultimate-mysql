0d2cc75bfd1ec1ce4ec4de08433aee58a2333ef1
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-26 21:01:02 -0400

    Create basic-logic-practice.sql
    Include comments for tasks to complete

---
+++ basic-logic-practice.sql
@@ -0,0 +1,15 @@
+-- Select all books written strictly before 1980
+
+-- Select all books written by Eggers or Chabon
+
+-- Written by Lahiri after 2000
+
+-- Pages 100-200
+
+-- Last name starts with C or S
+
+-- Title contains "stories" --> Short Stories
+-- "Just Kids" and "A Heartbreaking Work..." --> Memoirs
+-- Else --> Novel
+
+-- Get last name and number of books with "book" or "books" actually written out
