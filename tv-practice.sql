50185678fa095526d6e81a2c76a4621dfb0a6e66
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-03-29 18:35:29 -0400

    Create tv-practice.sql

---
+++ tv-practice.sql
@@ -0,0 +1,58 @@
+-- Query title (alphabetical) and rating (ascending) for all ratings
+SELECT S.title, Rv.rating
+FROM Series AS S
+	JOIN Reviews AS Rv
+		ON S.id = Rv.series_id
+ORDER BY S.title ASC, Rv.rating ASC;
+
+-- Query title (alphabetical) and average rating
+SELECT S.title, AVG(Rv.rating)
+FROM Series AS S
+	JOIN Reviews AS Rv
+		ON S.id = Rv.series_id
+GROUP BY S.title
+ORDER BY S.title ASC;
+
+-- Query all reviews with
+-- first_name, last_name, rating, series (becuase I want to)
+SELECT R.first_name, R.last_name, Rv.rating, S.title
+FROM (
+		Reviews AS R
+		JOIN Reviewers AS Rv
+			ON R.reviewer_id = Rv.id
+	 )
+	 JOIN Series AS S
+		ON R.series_id = S.id
+ORDER BY R.last_name ASC, R.first_name ASC, S.title ASC;
+
+-- Query unreviewed series
+SELECT S.title
+FROM Series AS S
+	RIGHT JOIN Reviews AS Rv
+		ON S.id = Rv.series_id
+WHERE Rv.rating IS NULL
+ORDER BY S.title ASC;
+	
+-- Query genre and average rating for genre
+SELECT S.genre, AVG(R.rating)
+FROM Series AS S
+	JOIN Reviews AS R
+		ON S.id = R.series_id
+ORDER BY S.genre ASC;
+
+-- Query reviewer statistics: first, last, how many reviews,
+-- min review, max review, avg review, status (active/inactive)
+SELECT 	R.first_name,
+		R.last_name,
+		COUNT(Rv.rating) AS ct,
+		MIN(Rv.rating),
+		MAX(Rv.rating),
+		AVG(Rv.rating),
+		CASE
+			WHEN ct = 0 THEN 'INACTIVE'
+			ELSE 'ACTIVE'
+		END AS status
+FROM Reviewers AS R
+	JOIN Reviews AS Rv
+		ON R.id = Rv.reviewer_id
+GROUP BY R.first_name, R.last_name;
