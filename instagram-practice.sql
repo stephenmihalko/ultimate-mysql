7bd4523fdcaf26824491bf9f078d37df29fc1d35
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-30 16:09:09 -0400

    Solve second challenge

--- instagram-practice.sql
+++ instagram-practice.sql
@@ -5,3 +5,10 @@ SELECT *
 FROM Users
 ORDER BY created_at ASC
 LIMIT 5;
+
+-- What day of the week do most users register on?
+SELECT DATE_FORMAT(created_at,'%W') AS sign_up_day,
+       COUNT(DATE_FORMAT(created_at,'%W')) AS count
+FROM Users
+GROUP BY sign_up_day
+ORDER BY count DESC;
