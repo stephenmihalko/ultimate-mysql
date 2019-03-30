b72852788462b5d5384bf106dc414ee3b4970050
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-30 16:22:16 -0400

    Solve challenge 3

--- instagram-practice.sql
+++ instagram-practice.sql
@@ -12,3 +12,10 @@ SELECT DAYNAME(created_at) AS sign_up_day,
 FROM Users
 GROUP BY sign_up_day
 ORDER BY count DESC;
+
+-- Find inactive users (who have never posted a photo)
+SELECT U.username
+FROM Users AS U
+     LEFT JOIN Photos AS P
+        ON U.id = P.user_id
+WHERE image_url IS NULL;
