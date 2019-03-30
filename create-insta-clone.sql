6b0d78b7a74610fe9733ff1e1cb4f9e245eff0a2
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-30 12:28:57 -0400

    Add users table

--- create-insta-clone.sql
+++ create-insta-clone.sql
@@ -1,1 +1,9 @@
 -- This allows for creation of an Instagram-like schema.
+
+-- Users table
+CREATE TABLE Users
+(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(15) UNIQUE NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
