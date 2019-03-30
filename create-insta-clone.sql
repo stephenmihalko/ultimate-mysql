9b1d40ebea617c1ca8bec84592d736501ee847f7
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-30 12:30:37 -0400

    Create Photos table

--- create-insta-clone.sql
+++ create-insta-clone.sql
@@ -7,3 +7,13 @@ CREATE TABLE Users
   username VARCHAR(15) UNIQUE NOT NULL,
   created_at TIMESTAMP DEFAULT NOW()
 );
+
+-- Photos table
+CREATE TABLE Photos
+(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  url VARCHAR(30) UNIQUE NOT NULL,
+  user_id NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY(user_id) REFERENCES Users(id)
+);
