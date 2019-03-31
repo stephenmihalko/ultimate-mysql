32869feabc9d9ae61bb78f8b4e05f6d5ebd208fe
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-31 16:36:04 -0400

    Complete first challenge

--- node-site/join-us-exercises.sql
+++ node-site/join-us-exercises.sql
@@ -1,5 +1,9 @@
 -- 1. Find the earliest date that a user joined as "Month ordinal YYYY"
 --    The field should be called "earliest_date"
+SELECT DATE_FORMAT(created_at, '%M %D %Y') AS earliest_date
+FROM Users
+ORDER BY created_at
+LIMIT 1;
 
 -- 2. Find the email of the earliest user
 
