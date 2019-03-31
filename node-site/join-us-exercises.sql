b80719f0a78e46b2719199b2845ec89e8e71a254
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-31 16:39:39 -0400

    Improve challenge 1 and solve challenge 2

--- node-site/join-us-exercises.sql
+++ node-site/join-us-exercises.sql
@@ -1,11 +1,12 @@
 -- 1. Find the earliest date that a user joined as "Month ordinal YYYY"
 --    The field should be called "earliest_date"
-SELECT DATE_FORMAT(created_at, '%M %D %Y') AS earliest_date
-FROM Users
-ORDER BY created_at
-LIMIT 1;
+SELECT DATE_FORMAT(MIN(created_at), '%M %D %Y') AS earliest_date
+FROM Users;
 
 -- 2. Find the email of the earliest user
+SELECT email
+FROM Users
+WHERE created_at = (SELECT MIN(created_at) FROM Users);
 
 -- 3. Print number of users joined by month
 
