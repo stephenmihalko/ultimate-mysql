6ed5e5f35eccde6187cbd68db84e88d1cf5775fe
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-31 16:30:33 -0400

    Create join-us-exercises.sql

---
+++ node-site/join-us-exercises.sql
@@ -0,0 +1,10 @@
+-- 1. Find the earliest date that a user joined as "Month ordinal YYYY"
+--    The field should be called "earliest_date"
+
+-- 2. Find the email of the earliest user
+
+-- 3. Print number of users joined by month
+
+-- 4. Count number of users with yahoo emails
+
+-- 5. Find the total users per email host
